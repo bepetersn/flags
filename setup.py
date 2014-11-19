51d9ee39f7a56636cd28998feadfd04d07be191f
Brian Peterson <bepetersn@gmail.com>
2014-11-19 03:51:27 -0600

    make a setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'flags',
+    packages = ['flags'],
+    version = '0.0.1',
+    description = 'Flag passer',
+    author='Brian Peterson',
+    author_email='bepetersn@gmail.com',
+    url='https://github.com/bepetersn/flags'
+)
