f7f5e7f19471390e60ecc4fe3e02f96fb5263931
Brian Peterson <bepetersn@gmail.com>
2014-11-19 23:45:00 -0600

    rename stuff; add an OK docstring; add better comments

--- flags.py
+++ flags.py
@@ -1,35 +1,39 @@
 import math 
 
 """
- 
-"""
+    Provides a module for packing and unpacking integer representations
+    of binary flags. 
+
+    `code` does the packing: it takes a list of integers, or "flags", 
+    and returns a unique code, a single integer. `flags` is the inverse
+    function, taking a code and unpacking it into flags. 
+
+    All the methods take an `index_base` param, which should be the 
+    base index for flags passed in, or unpacked from a code, i.e. 
+    0 for a zero-based index or 1 for a one-based index. """
     
 
-""" Returns the nth member of the 2^n sequence. """
-def seq(n, base=0):
-    return 2 ** (n-base)
+""" Returns the nth member of the y=2^(`n`-`index_base`) sequence. """
+def _seq(n, index_base=0):
+    return 2 ** (n-index_base)
 
 
-""" Retrieves the flag, or index, of the highest member of the
-    2^n sequence contained within a code. """
-def highest_flag(code, base=0):
-    return int(math.log(code, 2)) + base
+""" Retrieves the flag, or index, of the highest member of `_seq`
+    contained within `code`. """
+def _highest_flag(code, index_base=0):
+    return int(math.log(code, 2)) + index_base
 
 
-""" Returns the flags that make up your code. """
-def flags(code, base=0):
-    import pdb; pdb.set_trace()
+""" Returns the flags uniquely corresponding to `code`. """
+def flags(code, index_base=0):
     if code > 0:
-        highest = highest_flag(code, base)
-        return flags(code-seq(highest, base), base) + [highest]
+        highest = _highest_flag(code, index_base)
+        return flags((code - _seq(highest, index_base)), index_base) + [highest]
     return []
 
-""" Returns the members of the 2^n sequence that sum to your code. """
-def parts(code, base=0):
-    return [seq(f, base) for f in flags(code, base)]
 
+""" Returns the code uniquely corresponding to `flags`. """
+def code(flags, index_base=0):
+    return sum([_seq(f, index_base) for f in flags])
 
-""" Returns the code corresponding to a certain list of flags. """
-def code(flags, base=0):
-    return sum([seq(f, base) for f in flags])
     
